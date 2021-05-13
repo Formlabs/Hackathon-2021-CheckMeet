5da8e17c4f6ba1b3980be15ecfe800de8d66148e
Tamás PEREGI <tamas.peregi@formlabs.com>
2021-05-13 16:22:07 +0200

    Add fake driver

---
+++ service/driver_fake.py
@@ -0,0 +1,13 @@
+import json
+
+def load_json():
+    with open('fake.json', 'r') as f:
+        return json.load(f)
+
+def is_webcam_used():
+    obj = load_json()
+    return bool(obj['webcam'])
+
+def is_microphone_used():
+    obj = load_json()
+    return bool(obj['microphone'])
--- service/driver_mac.py
+++ service/driver_mac.py
@@ -1,7 +1,9 @@
+import driver_fake
+
 def is_webcam_used():
     # TODO
-    return False
+    return driver_fake.is_webcam_used()
 
 def is_microphone_used():
     # TODO
-    return False
+    return driver_fake.is_microphone_used()
