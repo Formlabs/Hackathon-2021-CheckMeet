f3cd1d39aa2f28358f49bb9bc1c0e552c43f534d
Tamás PEREGI <tamas.peregi@formlabs.com>
2021-05-13 15:33:18 +0200

    Send "shutdown" message on Ctrl+C

--- service/service.py
+++ service/service.py
@@ -19,6 +19,21 @@ def sendudp(ip, port, msg):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     sock.sendto(bytes(msg, 'utf-8'), (ip, port))
 
+def loopbody(args, counter):
+    shutdown = counter<0
+    msg = json.dumps({
+        "version": 1,
+        "webcam": not shutdown and is_webcam_used(),
+        "microphone": not shutdown and is_microphone_used(),
+    })
+    now = datetime.datetime.now()
+    ts = f'{now.hour:02}:{now.minute:02}:{now.second:02}'
+    print(f'[{ts}] {msg}')
+    if shutdown or counter % args.send_rate == 0:
+        print('Sending over UDP')
+        for ip in args.ip:
+            sendudp(ip, args.port, msg)
+
 def main():
     parser = argparse.ArgumentParser(description='Service')
     parser.add_argument('--port', default=26999, type=int, help='Use UDP port')
@@ -27,20 +42,14 @@ def main():
     parser.add_argument('ip', nargs='+', help='Send UDP packets to these IP adresses')
     args = parser.parse_args()
 
-    for counter in itertools.count(start=0):
-        msg = json.dumps({
-            "version": 1,
-            "webcam": is_webcam_used(),
-            "microphone": is_microphone_used(),
-        })
-        now = datetime.datetime.now()
-        ts = f'{now.hour:02}:{now.minute:02}:{now.second:02}'
-        print(f'[{ts}] {msg}')
-        if counter % args.send_rate == 0:
-            print('Sending over UDP')
-            for ip in args.ip:
-                sendudp(ip, args.port, msg)
-        time.sleep(args.query_interval)
+    try:
+        for counter in itertools.count(start=0):
+            loopbody(args, counter)
+            time.sleep(args.query_interval)
+    except KeyboardInterrupt as e:
+        print('User pressed Ctrl+C, aborting')
+        loopbody(args, -1)
+        raise
 
 if __name__ == '__main__':
     main()
