6f1e0bbf06301c7a26e11335baaea401daa393a6
Tamás PEREGI <tamas.peregi@formlabs.com>
2021-05-14 18:24:21 +0200

    Solve #4: support Linux

--- service/driver_auto.py
+++ service/driver_auto.py
@@ -4,6 +4,8 @@ if platform.system()=='Windows':
     from driver_win import is_webcam_used, is_microphone_used
 elif platform.system()=='Darwin':
     from driver_mac import is_webcam_used, is_microphone_used
+elif platform.system()=='Linux':
+    from driver_linux import is_webcam_used, is_microphone_used
 else:
     print('This platform is not supported')
     sys.exit(2)
---
+++ service/driver_linux.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def is_used(device_path):
+    p = subprocess.run(f'fuser {device_path}',
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
+    return p.stdout != ''
+
+def is_microphone_used():
+    return is_used('/dev/snd/pcmC*D*c')
+
+def is_webcam_used():
+    return is_used('/dev/video*')
